58cfda0bd13212c94b72e099bbb5c24788692628
lucas <lucasatwon@gmail.com>
2018-03-19 23:57:54 +0100

    Connect Project with its tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,9 @@ class TasksController < ApplicationController
   	 def index
 	    @title = "Tasks"
 	    #@Tasks = Task.order("created_at DESC")
-	    @Tasks = Task.where(:status => nil).order("created_at DESC")
-	    @Tasks_sip = Task.where(:status => false)
-	    @Tasks_completed = Task.where(status: true)
+	    @Tasks = Task.where(:status => nil, :project_id => params[:project_id]).order("created_at DESC")
+	    @Tasks_sip = Task.where(:status => false, :project_id => params[:project_id])
+	    @Tasks_completed = Task.where(status: true, :project_id => params[:project_id])
 
 	    @new_task = Task.new
 	  end
@@ -13,19 +13,19 @@ class TasksController < ApplicationController
 	  def create
 	  	@task = Task.new(task_params)
 	  	if @task.save
-	  		redirect_to :action => 'task', :notice => "The task has been successfully updated."
-	  		respond_to do |f|
-			   f.html { redirect_to tasks_url }
-			   f.js
-		  	end
+	  		redirect_to :action => 'index', :notice => "The task has been successfully updated."
+	  		#respond_to do |f|
+			#   f.html { redirect_to projects_index_url }
+			#   f.js
+		  	#end
 	  	else
-	  		render action: "task", :notice => "Failed please check your input."
+	  		render action: "index", :notice => "Failed please check your input."
 	  	end
 	  end
 
 	  def delete
 	  	Task.find(params[:id]).destroy
-    	redirect_to :action => 'task'
+    	redirect_to :action => 'index'
 	  end
 
 	  private
